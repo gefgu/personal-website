6eb3c146bb6cb7c76644926fbd4998d0849bbbbc
Gustavo Santos <gefgu@hotmail.com>
2022-12-24 09:07:55 -0300

    feat: add title to layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -8,5 +8,13 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  markdown: {
+    drafts: true,
+  },
+  integrations: [
+    tailwind(),
+    mdx({
+      drafts: true,
+    }),
+  ],
 });
